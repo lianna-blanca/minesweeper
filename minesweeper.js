36deda396866e5b47e92ef966e6cb92cb82914e7
lianna-blanca <lianna.blanca@gmail.com>
2018-06-10 16:04:28 +1200

    Adding auto generate for cells (not working yet)

--- minesweeper.js
+++ minesweeper.js
@@ -2,18 +2,32 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
 
-var isMine
-var hidden
-
 var board = {
-	cells: [
-	{row : 0, col: 0, isMine: true, hidden: true}, 
-	{row : 0, col: 1, isMine: true, hidden: true}, 
-	{row : 1, col: 0, isMine: false, hidden: true}, 
-	{row : 1, col: 1, isMine: false, hidden: true}]
+	cells: []
+	}
+
+var boardSize = 2;
+
+function gameBoard() {
+	for (var v = 0; v < boardSize; v++) {
+		for (var h = 0; h < boardSize; h++) {
+			board.cells.push({
+				row: r,
+				col: c,
+				isMine: false, //change later
+				isMarked: false,
+				hidden: true,
+			})
+		}
+	}
 }
 
+
 function startGame () {
+  for (i = 0, i <= board.cells.length; i++;) {
+  	board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+  }
+
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
@@ -38,4 +52,6 @@ function checkForWin () {
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
+	
+	var surroundingCells = getSurroundingCells(row, col);
 }
